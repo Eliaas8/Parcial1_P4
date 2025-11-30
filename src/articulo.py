31bdeedfcbc1adbfbe314674d32c15ee64d658cd
Eliaas8 <eggm0108@gmail.com>
2025-11-29 21:58:52 -0500

    Add Articulo class for budget items

--- src/articulo.py
+++ src/articulo.py
@@ -1,1 +1,16 @@
+class Articulo:
+    # Modelo de un artículo del presupuesto
+    def __init__(self, id, nombre, categoria, cantidad, precio_unit, descripcion):
+        self.id = id
+        self.nombre = nombre
+        self.categoria = categoria
+        self.cantidad = cantidad
+        self.precio_unit = precio_unit
+        self.descripcion = descripcion
+
+    def total(self):
+        return self.cantidad * self.precio_unit
+
+    def a_dict(self):
+        return self.__dict__
 
